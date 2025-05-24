b3d4f97d8bf90f2aec97a25e8cb49a6658af81c6
Bruno Breggia <breggiabruno@gmail.com>
2025-05-24 14:15:09 -0300

    Cambio nombres de carpetas

--- nussinov_complexity.py
+++ nussinov_complexity.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 tiempos = []
 
-with open("time_analysis_nussinov.txt", "r") as file:
+with open("Matematica/time_analysis_nussinov.txt", "r") as file:
     for line in file:
         if "Duracion: " == line[:10]:
             tiempos.append(float(line[10:].strip('\n')))
